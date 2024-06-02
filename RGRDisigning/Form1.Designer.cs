7a4e88430e11b8e947dfe6e1037158e7e368232a
Danil <shashkoff.danil2016@yandex.ru>
2024-06-02 22:31:23 +0600

    Добавьте файлы проекта.

---
+++ RGRDisigning/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace RGRDisigning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWords = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtWords
+            // 
+            this.txtWords.Location = new System.Drawing.Point(109, 14);
+            this.txtWords.Name = "txtWords";
+            this.txtWords.Size = new System.Drawing.Size(642, 20);
+            this.txtWords.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(28, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "button1";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(28, 41);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(723, 397);
+            this.txtResults.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtWords);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWords;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ RGRDisigning/Form1.cs
@@ -0,0 +1,132 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGRDisigning
+{
+    public partial class Form1 : Form
+    {
+        private const int MaxPagesToVisit = 1000; // Максимальное количество страниц для посещения
+        private int pagesVisited;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string url = "https://ru.wikipedia.org/wiki/Сисигамбис"; // Замените на нужный URL
+            string words = txtWords.Text;
+            List<string> wordList = words.Split(new[] { ' ', ',', ';' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            pagesVisited = 0; // Сброс счетчика посещенных страниц
+            List<string> foundWordsAndLinks = CrawlWebPage(url, wordList, 2); // Начальная страница и глубина обхода
+
+            txtResults.Text = string.Join(Environment.NewLine, foundWordsAndLinks);
+        }
+
+        private List<string> CrawlWebPage(string url, List<string> words, int depth)
+        {
+            HashSet<string> visited = new HashSet<string>();
+            List<(string Url, Dictionary<string, int> WordCounts)> results = new List<(string, Dictionary<string, int>)>();
+
+            void Crawl(string currentUrl, int currentDepth)
+            {
+                if (currentDepth > depth || visited.Contains(currentUrl) || pagesVisited >= MaxPagesToVisit) return;
+                visited.Add(currentUrl);
+                pagesVisited++;
+
+                var wordsOnPage = ParseWebPage(currentUrl, words);
+                if (wordsOnPage.Count > 0)
+                {
+                    results.Add((currentUrl, wordsOnPage));
+                }
+
+                HtmlWeb web = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument doc = web.Load(currentUrl);
+
+                foreach (string link in GetLinksFromPage(doc.DocumentNode))
+                {
+                    if (pagesVisited >= MaxPagesToVisit) break; // Прерывание при достижении лимита
+                    Crawl(link, currentDepth + 1);
+                }
+            }
+
+            Crawl(url, 0);
+
+            // Сортировка результатов по количеству встреченных слов
+            var sortedResults = results
+                .OrderByDescending(result => result.WordCounts.Values.Sum())
+                .Select(result => $"Visited: {result.Url} - {string.Join(", ", result.WordCounts.Select(kv => $"{kv.Key}: {kv.Value} times"))}")
+                .ToList();
+
+            return sortedResults;
+        }
+
+        private Dictionary<string, int> ParseWebPage(string url, List<string> words)
+        {
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(url);
+
+            string text = GetTextFromHtml(doc.DocumentNode);
+            List<string> textWords = text.Split(new[] { ' ', ',', ';' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            foreach (var word in words)
+            {
+                int count = textWords.Count(tw => tw.Equals(word, StringComparison.OrdinalIgnoreCase));
+                if (count > 0)
+                {
+                    wordCounts[word] = count;
+                }
+            }
+
+            return wordCounts;
+        }
+
+        private string GetTextFromHtml(HtmlNode node)
+        {
+            if (node == null)
+                return string.Empty;
+
+            if (node.NodeType == HtmlNodeType.Text)
+                return node.InnerText;
+
+            return string.Join(" ", node.ChildNodes.Select(child => GetTextFromHtml(child)));
+        }
+
+        private List<string> GetLinksFromPage(HtmlNode node)
+        {
+            List<string> links = new List<string>();
+
+            foreach (HtmlNode link in node.SelectNodes("//a[@href]"))
+            {
+                HtmlAttribute attr = link.Attributes["href"];
+                if (attr != null)
+                {
+                    string hrefValue = attr.Value;
+                    if (Uri.IsWellFormedUriString(hrefValue, UriKind.RelativeOrAbsolute))
+                    {
+                        Uri baseUri = new Uri("https://ru.wikipedia.org");
+                        Uri absoluteUri = new Uri(baseUri, hrefValue);
+                        links.Add(absoluteUri.ToString());
+                    }
+                }
+            }
+
+            return links;
+        }
+    }
+}
